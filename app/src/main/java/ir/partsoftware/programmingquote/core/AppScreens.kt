466abeb9126d554192d4a9a1175da540299910c5
Javad Jafari <javad2147@yahoo.com>
2023-07-13 13:05:27 +0330

    #7 Implemented the AuthorsListScreen and fix navigation to this screen

--- app/src/main/java/ir/partsoftware/programmingquote/core/AppScreens.kt
+++ app/src/main/java/ir/partsoftware/programmingquote/core/AppScreens.kt
@@ -3,4 +3,10 @@ package ir.partsoftware.programmingquote.core
 sealed class AppScreens(val route: String) {
 
     object AuthorsList : AppScreens("authors-list")
+
+    object QuotesList : AppScreens("quotes-list/{id}") {
+        fun createRoute(id: Int): String {
+            return "quotes-list/$id"
+        }
+    }
 }
