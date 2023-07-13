8cede6d388917a656d72e563e1b52f95a95dd6f3
Javad Jafari <javad2147@yahoo.com>
2023-07-13 12:30:41 +0330

    #5 remove is dark from theme

--- app/src/main/java/ir/partsoftware/programmingquote/ui/theme/Theme.kt
+++ app/src/main/java/ir/partsoftware/programmingquote/ui/theme/Theme.kt
@@ -1,24 +1,13 @@
 package ir.partsoftware.programmingquote.ui.theme
 
 import android.app.Activity
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
-
-private val darkColorPalette = darkColors(
-    primary = Navy,
-    onPrimary = White700,
-    background = White500,
-    onBackground = SemiBlack,
-    surface = LightGray,
-    onSurface = Navy
-)
 
 private val lightColorPalette = lightColors(
     primary = Navy,
@@ -29,17 +18,11 @@ private val lightColorPalette = lightColors(
     onSurface = Navy
 )
 
-
 @Composable
 fun ProgrammingQuoteTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        darkColorPalette
-    } else {
-        lightColorPalette
-    }
+    val colors = lightColorPalette
 
     val view = LocalView.current
     if (!view.isInEditMode) {
@@ -47,9 +30,8 @@ fun ProgrammingQuoteTheme(
             val window = (view.context as Activity).window
             window.statusBarColor = colors.background.toArgb()
             window.navigationBarColor = colors.background.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
-            WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars =
-                !darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
+            WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars = true
         }
     }
 
